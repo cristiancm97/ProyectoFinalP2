476a407ee3bae87375e97a9732d30ea55dd3c376
cristiancm97git <cristian_cm_16@outlook.com>
2024-07-23 16:40:33 -0300

    7mo - Agregado validacion login

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
+// Obtener el formulario por su id
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
-    window.location.href = 'menuAdmin.html'; // Redirige a la otra página
+    event.preventDefault(); // Evitar que se envíe el formulario de manera predeterminada
+
+    // Obtener los valores de los inputs
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Llamar a la función para verificar el usuario
+    verificarUser(username, password);
 });
+
+// Función para verificar el usuario
+function verificarUser(username, password) {
+    fetch('http://localhost:3000/users')
+        .then(response => response.json()) // Convertir la respuesta en JSON
+        .then(users => {
+            // Buscar el usuario con el nombre de usuario y contraseña proporcionados
+            const user = users.find(user => user.usuario === username && user.contrasena === password);
+
+            if (user) {
+                // Verificar el rol del usuario y redireccionar
+                if (user.role === 'admin') {
+                    window.location.href = 'menuAdmin.html'; // Redireccionar a la página del administrador
+                } else if (user.role === 'empleado') {
+                    window.location.href = 'empleado.html'; // Redireccionar a la página del empleado
+                } else {
+                    alert('Rol de usuario desconocido');
+                }
+            } else {
+                alert('Usuario inválido');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Error al verificar el usuario');
+        });
+}
+
